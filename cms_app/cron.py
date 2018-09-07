1a9fd6c61957cc53c3c0f44d25125013c52ee61c
Stoiczkow <pawelwyzykowski@gmail.com>
2018-09-07 11:59:26 +0200

    add code to cron

--- cms_app/cron.py
+++ cms_app/cron.py
@@ -3,9 +3,7 @@ import glob
 import os
 import re
 
-
 today = str(datetime.datetime.now().date()).replace('-', '')
-
 os.chdir("C:/Users/v-pawel.wyzykowski/Desktop")
 file_name = ''
 
@@ -14,4 +12,25 @@ for i in glob.glob("*.txt"):
         file_name = i
 
 with open(file_name) as file:
-    print(file.readlines()[1])
+    interruptions = {}
+    all_lines = file.readlines()
+    current_interruption = ''
+    for j in range(1, len(all_lines)):
+        line = all_lines[j]
+        line_without_date = line[20:]
+        found = re.findall(r'([0-9]+)', line_without_date)
+
+        machine_columns = [0, 1]
+        if not int(found[0]):
+            if not current_interruption:
+                current_interruption = str(line[0:19])
+            try:
+                interruptions[current_interruption][0] += 1
+            except KeyError:
+                interruptions[current_interruption] = [1]
+        else:
+            try:
+                interruptions[current_interruption].append(str(line[0:19]))
+            except KeyError:
+                pass
+                current_interruption = ''
